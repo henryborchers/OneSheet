b79cacb8071eb6fa050e3b19eca068e5af285a5d
henryborchers <henryborchers@yahoo.com>
2015-03-03 15:35:01 -0800

    added test for video object

--- onesheet/test_videoObject.py
+++ onesheet/test_videoObject.py
@@ -1,7 +1,79 @@
 from unittest import TestCase
-
+from onesheet.VideoObject import *
 __author__ = 'lpsdesk'
 
 
 class TestVideoObject(TestCase):
+
+    def setUp(self):
+        self.f = VideoObject("/Users/lpsdesk/PycharmProjects/PBcore/sample_records/casjhsj_000061/casjhsj_000061_r1_prsv.mov")
+
+    def test_getVideoCodec(self):
+        self.fail()
+
+    def test_getVideoCodecLongName(self):
+        self.fail()
+
+    def test_getVideoCodecTagString(self):
+        self.fail()
+
+    def test_getVideoCodecTag(self):
+        self.fail()
+
+    def test_getVideoFrameRate(self):
+        self.fail()
+
+    def test_getVideoColorSpace(self):
+        self.fail()
+
+    def test_getVideoColorSampling(self):
+        self.fail()
+
+    def test_getVideoBitRate(self):
+        self.fail()
+
+    def test_getVideoBitRate(self):
+        self.fail()
+
+    def test_getVideoBitRateH(self):
+        self.fail()
+
+    def test_getVideoResolution(self):
+        self.fail()
+
+    def test_getVideoResolutionHeight(self):
+        self.fail()
+
+    def test_getVideoRespolutionWidth(self):
+        self.fail()
+
+    def test_getFilePath(self):
+        self.assertEqual(self.f.file_path, "/Users/lpsdesk/PycharmProjects/PBcore/sample_records/casjhsj_000061")
+
+    def test_getFormatType(self):
+        self.assertEqual(self.f.format_type, "video")
+
+    def test_getFileExtension(self):
+        self.assertEqual(self.f.file_extension, '.mov')
+
+    def test_getFileSize(self):
+        self.assertEqual(self.f.file_size, 6531678142)
+
+    def test_getFileSizeH(self):
+        self.assertEqual(self.f.file_size_human, "6.1 GB")
+
+    def test_getDateCreated(self):
+        self.assertEqual(self.f.date_created, "Thu Feb 26 16:09:24 2015")
+
+    def test_getDateLastModified(self):
+        self.assertEqual(self.f.date_last_modified, "Thu Jan 15 11:18:30 2015")
+
+    def test_getTotalRunningTimeRaw(self):
+        self.assertEqual(self.f.getTotalRunningTimeRaw(), "ddd")
+
+    def test_getTotalRunningTimeSMPTE(self):
+        self.fail()
+
+    def test_getTotalSeconds(self):
+        self.fail()
     pass

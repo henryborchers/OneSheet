eefede69ea2c9234d5f224ac393f499a648a1e8d
henryborchers <henryborchers@yahoo.com>
2015-03-03 11:44:09 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='OneSheet',
+    version='0.1',
+    description='Easily access metadata for image, video, sound, and document file.',
+    url='https://github.com/henryborchers/OneSheet',
+    license='GPL3',
+    author='Henry Borchers',
+    author_email='henryborchers@yahoo.com',
+    packages=['onesheet'],
+)

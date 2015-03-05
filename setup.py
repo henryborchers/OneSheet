a62e4b230d643cb84b5d3e3456cddf1eaee05c65
henryborchers <henryborchers@yahoo.com>
2015-03-05 15:45:40 -0800

    added version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup
 
 setup(
     name='OneSheet',
-    version='0.1.3.4',
+    version='0.1.3.7',
     description='Easily access metadata for image, video, sound, and document file.',
     long_description=__doc__,
     url='https://github.com/henryborchers/OneSheet',

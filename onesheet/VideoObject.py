d114c2914ca46b2ac32e0f27753d38a936198a97
henryborchers <henryborchers@yahoo.com>
2015-03-04 17:41:57 -0800

    Added audio metadata to videoObject

--- onesheet/VideoObject.py
+++ onesheet/VideoObject.py
@@ -6,7 +6,7 @@ from onesheet.VideoMetadata import VideoMetadata
 from onesheet.AudioMetadata import AudioMetadata
 
 # class VideoObject(AudioMetadata, VideoMetadata, XMPVideoMetadata):
-class VideoObject(VideoMetadata):
+class VideoObject(VideoMetadata, AudioMetadata):
 
     def __init__(self, filename):
         VideoMetadata.__init__(self, filename)

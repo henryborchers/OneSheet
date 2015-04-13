a86a1200f0bd3f30e6e5096c38974a35061a13a3
henryborchers <henryborchers@yahoo.com>
2015-04-13 13:08:45 -0700

    Duration now pulls from the stderr instead of the audiostream

--- onesheet/TimeBasedMetadata.py
+++ onesheet/TimeBasedMetadata.py
@@ -11,12 +11,15 @@ class TimeBasedMetadata(FileMetadata):
     __metaclass__ = ABCMeta
     def __init__(self, filename):
         FileMetadata.__init__(self, filename)
-        self.GET_XML_COMMAND = 'ffprobe -print_format xml -show_streams'.split()
+        self.raw_stderr = ""
+        GET_XML_COMMAND = 'ffprobe -print_format xml -show_streams'.split()
         self.fileName = filename
         if self.format_type == 'audio' or self.format_type == 'video':
-            command = self.GET_XML_COMMAND
+            command = GET_XML_COMMAND
             command.append(self.fileName)
-            p = Popen(command, stdout=PIPE, stderr=STDOUT, bufsize=0)
+            p = Popen(command, stdout=PIPE, stderr=PIPE, bufsize=0)
+            for line in p.stderr.readlines():
+                self.raw_stderr += line
             self.rawdata = p.communicate()[0]
             self.fileXML = str(search('(<\?xml).*(</ffprobe>)', self.rawdata, DOTALL).group(0))
             self.xmlDom = parseString(self.fileXML)
@@ -38,14 +41,19 @@ class TimeBasedMetadata(FileMetadata):
     @property
     def totalRunningTimeSMPTE(self):
 
-        for stream in self.xmlDom.getElementsByTagName('stream'):
-                if stream.getAttribute("codec_type") == "audio":
-                    seconds = float(stream.getAttribute("duration"))#TODO create SMPTE TRT return
-                    break
-
-        m, s = divmod(seconds, 60)
-        h, m = divmod(m, 60)
-        return str(int(h)).zfill(2) + ":" + str(int(m)).zfill(2) + ":" + str(int(s)).zfill(2)
+        # for stream in self.xmlDom.getElementsByTagName('stream'):
+        #     if stream.getAttribute("codec_type") == "audio":
+        #         seconds = float(stream.getAttribute("duration"))#TODO create SMPTE TRT return
+        #         break
+        #
+        # m, s = divmod(seconds, 60)
+        # h, m = divmod(m, 60)
+        # return str(int(h)).zfill(2) + ":" + str(int(m)).zfill(2) + ":" + str(int(s)).zfill(2)
+        REGEX_DURATION_PATTERN = '(?<=(Duration: ))\d\d:\d\d:\d\d.\d\d(?=,)'
+        # print self.raw_stderr
+        results = search(REGEX_DURATION_PATTERN, self.raw_stderr)
+        # print
+        return results.group(0)
         pass
 
     @property

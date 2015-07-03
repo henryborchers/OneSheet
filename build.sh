244d645c41ac473fdce040b93a2f2d767f6946e6
henryborchers <henryborchers@yahoo.com>
2015-07-02 17:03:52 -0700

    Works in python 3 now!

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#if [ ! -f /tmp/lame.tar.gz ]; then
+#    echo "Downloading LAME";
+#    wget -O /tmp/lame.tar.gz http://downloads.sourceforge.net/project/lame/lame/3.98.4/lame-3.98.4.tar.gz;
+#else
+#    echo "Found LAME";
+#fi
+
+if [ ! -d /tmp/ffmpeg ]; then
+    git clone git://source.ffmpeg.org/ffmpeg.git /tmp/ffmpeg
+else
+    echo "Found FFMPEG"
+fi
+
+/tmp/ffmpeg/configure --prefix=${PREFIX} --disable-ffmpeg  --disable-ffplay --disable-ffserver --disable-everything --disable-doc --enable-small;
+make;
+make install;
+#make clean;
+
+
+
+
+$PYTHON setup.py install
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.

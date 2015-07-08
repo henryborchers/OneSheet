e5a12e6684e61c4b8fb7c73a373a5cc09eec433f
henryborchers <henryborchers@yahoo.com>
2015-07-08 16:08:52 -0700

    fixed name FileObject instead of Timebasedmetata

--- onesheet/FileObject.py
+++ onesheet/FileObject.py
@@ -2,7 +2,7 @@ __author__ = 'California Audio Visual Preservation Project'
 from abc import ABCMeta
 from onesheet.ClassFileMetadata import FileMetadata
 
-class TimeBasedMetadata(FileMetadata):
+class FileObject(FileMetadata):
     __metaclass__ = ABCMeta
     def __init__(self, filename):
         FileMetadata.__init__(self, filename)
